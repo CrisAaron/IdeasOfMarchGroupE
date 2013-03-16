94867086a76b1785b1a70959ea2c6ede9b0e302c
lamont92 <alamont@csumb.edu>
2013-03-16 12:54:54 -0700

    Resources

--- gen/com/example/csumb_spa/R.java
+++ gen/com/example/csumb_spa/R.java
@@ -24,18 +24,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int Header=0x7f080002;
         public static final int TestButton=0x7f080000;
-        public static final int action_settings=0x7f080003;
-        public static final int mainmenucallbutton=0x7f080002;
+        public static final int Text=0x7f080003;
+        public static final int action_settings=0x7f080005;
+        public static final int mainmenucallbutton=0x7f080004;
         public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_help_main=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int activity_signs_2=0x7f030002;
-        public static final int activity_signs_3=0x7f030003;
-        public static final int activity_signs__main=0x7f030004;
-        public static final int main_menu=0x7f030005;
+        public static final int activity_resources=0x7f030002;
+        public static final int activity_resources2=0x7f030003;
+        public static final int activity_resources3=0x7f030004;
+        public static final int activity_resources4=0x7f030005;
+        public static final int activity_resources5=0x7f030006;
+        public static final int activity_signs_2=0x7f030007;
+        public static final int activity_signs_3=0x7f030008;
+        public static final int activity_signs__main=0x7f030009;
+        public static final int main_menu=0x7f03000a;
     }
     public static final class menu {
         public static final int main=0x7f070000;
